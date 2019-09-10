eb9999e31850af6a4af915704f7e261d569aac7e
Mosh Feu <moshfeu@gmail.com>
2019-09-10 19:34:23 +0300

    diff only first file

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { compareSync } from 'dir-compare';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,11 +17,51 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		compare();
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function openFolder(): Promise<string> {
+  const options: vscode.OpenDialogOptions = {
+    canSelectMany: false,
+    openLabel: 'Open',
+    canSelectFolders: true,
+  };
+
+  return new Promise((resolve) => {
+    vscode.window.showOpenDialog(options).then(fileUri => {
+      if (fileUri && fileUri[0]) {
+          resolve(fileUri[0].fsPath);
+      }
+    });
+  });
+}
+
+async function compare() {
+  const folder1 = await openFolder();
+  const folder2 = await openFolder();
+
+  const options = {compareContent: true};
+  const res = compareSync(
+    folder1,
+    folder2,
+    options
+  );
+
+  const { diffSet = [] } = res;
+
+  const diffs = diffSet
+    .filter(diff => diff.state === 'distinct')
+    .map(diff => [`${diff.path1}/${diff.name1}`, `${diff.path2}/${diff.name2}`]);
+
+  let success = await vscode.commands.executeCommand('vscode.diff',
+    vscode.Uri.file(diffs[0][0]),
+    vscode.Uri.file(diffs[0][1]),
+    'My great Diff'
+  );
+  // console.log(success);
 }
 
 // this method is called when your extension is deactivated
