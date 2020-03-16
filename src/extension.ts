62096473af8ec7bd05e2ac8fc25d942bba1a47da
llgcode <legoff.laurent@gmail.com>
2020-03-16 14:47:03 +0100

    Switch left and right (#21)

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,20 @@
-import { window, commands, Disposable, env, Uri } from 'vscode';
+import { window, commands, ExtensionContext} from 'vscode';
 import { CompareFoldersProvider } from './providers/foldersCompareProvider';
 import { COMPARE_FILES, CHOOSE_FOLDERS_AND_COMPARE, REFRESH } from './constants/commands';
 import { ViewOnlyProvider } from './providers/viewOnlyProvider';
 
-const disposables: Disposable[] = [];
-export function activate() {
+
+export function activate(context: ExtensionContext) {
   const onlyInA = new ViewOnlyProvider();
   const onlyInB = new ViewOnlyProvider();
   const foldersCompareProvider = new CompareFoldersProvider(onlyInA, onlyInB);
-
-  disposables.push(
-    ...[
+  context.subscriptions.push(
       window.registerTreeDataProvider('foldersCompareAppService', foldersCompareProvider),
       window.registerTreeDataProvider('foldersCompareAppServiceOnlyA', onlyInA),
       window.registerTreeDataProvider('foldersCompareAppServiceOnlyB', onlyInB),
       commands.registerCommand(COMPARE_FILES, foldersCompareProvider.onFileClicked),
       commands.registerCommand(CHOOSE_FOLDERS_AND_COMPARE, foldersCompareProvider.chooseFoldersAndCompare),
       commands.registerCommand(REFRESH, foldersCompareProvider.refresh),
-    ]
   );
 }
 
-export function deactivate() {
-  disposables.forEach(disposable => disposable.dispose());
-}
