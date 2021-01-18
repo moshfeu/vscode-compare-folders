4f9b76848344cf6ef92bfcfdf5ded6ebb372cde9
Mosh Feu <moshfeu@gmail.com>
2021-01-18 23:50:15 +0200

    add error message + report

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -2,11 +2,13 @@ import { window } from 'vscode';
 
 const logger = window.createOutputChannel('Compare Folders');
 
-export function log(data: object | string | undefined) {
-  if (typeof data === 'string') {
-    logger.appendLine(data);
-  } else {
-    logger.appendLine(JSON.stringify(data, null, 2));
-  }
-  console.log(data);
+export function log(...data: (object | string | undefined)[]) {
+  data.forEach(item => {
+    if (typeof item === 'string') {
+      logger.appendLine(item);
+    } else {
+      logger.appendLine(JSON.stringify(item, null, 2));
+    }
+  });
+  console.log(...data);
 }
--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -1,29 +1,61 @@
-import { window, ProgressLocation } from 'vscode';
+import { window, ProgressLocation, env, Uri } from 'vscode';
+import { log } from '../services/logger';
+import os from 'os';
+import { version } from '../../package.json';
 
 export function showInfoMessageWithTimeout(message: string, timeout: number = 3000) {
   const upTo = timeout / 10;
-  window.withProgress({
-    location: ProgressLocation.Notification,
-    title: message,
-    cancellable: true
-  },
-  async (progress) => {
-    let counter = 0;
-    return new Promise<void>((resolve) => {
-      const interval = setInterval(() => {
-        progress.report({increment: counter / upTo});
-        if (++counter === upTo) {
-          clearInterval(interval);
-          resolve();
-        }
-      }, 10);
-    });
-  });
+  window.withProgress(
+    {
+      location: ProgressLocation.Notification,
+      title: message,
+      cancellable: true,
+    },
+    async (progress) => {
+      let counter = 0;
+      return new Promise<void>((resolve) => {
+        const interval = setInterval(() => {
+          progress.report({ increment: counter / upTo });
+          if (++counter === upTo) {
+            clearInterval(interval);
+            resolve();
+          }
+        }, 10);
+      });
+    }
+  );
 }
 
 export async function showDoneableInfo(title: string, callback: () => Promise<void>) {
-  await window.withProgress({
-    location: ProgressLocation.Notification,
-    title,
-  }, async () => callback());
+  await window.withProgress(
+    {
+      location: ProgressLocation.Notification,
+      title,
+    },
+    async () => callback()
+  );
+}
+
+export async function showErrorMessage(message: string, error: any) {
+  if ((await window.showErrorMessage(message, 'Report')) === 'Report') {
+    try {
+      const body = `**Original message**: ${message}
+
+**System Info**
+Extension version: ${version}
+OS: ${os.platform()} ${os.release()}
+
+**Stack**
+${error.stack || error.message || error}
+`;
+      const url = `https://github.com/moshfeu/vscode-compare-folders/issues/new?title=[error] ${
+        error.message || error
+      }&body=${body}`;
+
+      const uri = Uri.parse(url);
+      env.openExternal(uri);
+    } catch (error) {
+      log(error);
+    }
+  }
 }
