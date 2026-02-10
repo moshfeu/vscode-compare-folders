78a5ad431e8508cb31d5e9071cb58fdc90ed8078
Mosh Feu <moshfeu@gmail.com>
2026-02-10 23:21:32 +0200

    Add default timeout for parsing rules and export constant for execution timeout

--- src/services/fileParser.ts
+++ src/services/fileParser.ts
@@ -4,6 +4,7 @@ import minimatch from 'minimatch';
 import { workspace } from 'vscode';
 import { FileParsingRule, getConfiguration } from './configuration';
 import { log } from './logger';
+import { PARSING_RULE_EXECUTION_TIMEOUT } from '../utils/consts';
 
 const execFileAsync = promisify(execFile);
 
@@ -61,7 +62,7 @@ async function executeParsingCommand(filePath: string, rule: FileParsingRule): P
   );
 
   const workingDirectory = rule.workingDirectory || process.cwd();
-  const timeout = rule.timeout || 30000;
+  const timeout = rule.timeout || PARSING_RULE_EXECUTION_TIMEOUT;
   const env = { ...process.env, ...rule.env };
 
   log(`Executing parsing command: ${rule.command} ${args.join(' ')} in ${workingDirectory}`);
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,3 +2,4 @@ export const GLOB_ROOT = '*';
 export const GIT_FOLDER = '.git';
 export const YES_MESSAGE = 'Yes, go for it';
 export const READONLY_SCHEME = 'compare-folders-readonly';
+export const PARSING_RULE_EXECUTION_TIMEOUT = 30000;
