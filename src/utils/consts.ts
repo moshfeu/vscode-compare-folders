da9d8462633ca29549b7cd9ce33f98e285613dfc
justcontributor <dumkty5663@gmail.com>
2026-04-05 18:00:45 +0900

    Add missing string

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,5 +1,6 @@
+import { l10n } from 'vscode';
 export const GLOB_ROOT = '*';
 export const GIT_FOLDER = '.git';
-export const YES_MESSAGE = 'Yes, go for it';
+export const YES_MESSAGE = l10n.t('ui.yes');
 export const READONLY_SCHEME = 'compare-folders-readonly';
 export const PARSING_RULE_EXECUTION_TIMEOUT = 30000;
